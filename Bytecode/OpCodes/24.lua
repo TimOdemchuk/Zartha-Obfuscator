1206d06425d7084dbad760fed17a37d67e3cf7cc
TimOdemchuk <markosentir@gmail.com>
2026-01-27 15:09:41 -0800

    Redone opcode implementations for LT, LE, and TFORPREP; enhance input handling and update known issues

--- Bytecode/OpCodes/24.lua
+++ Bytecode/OpCodes/24.lua
@@ -5,12 +5,15 @@ return function(instruction,shiftAmount,constant,settings)
 	local reg_c = _G.getReg(instruction,"C",true)
 	
 	local layout = ([=[
-	  if (%s and Constants[:B:+1] or Stack[:B:]) %s (%s and Constants[:C:+1] or Stack[:C:]) then
-            pointer = pointer + 1
-        end
-	]=]):format(tostring(reg_b.k),_G.getReg(instruction,"A") >0 and ">" or "<",tostring(reg_c.k))
-	
+		if (%s %s %s) then
+			pointer = pointer + 1
+		end
+	]=]):format(
+		reg_b.k and "Constants[:B:+1]" or "Stack[:B:]", -- Check 1 (if A)
+		reg_a >0 and ">" or "<", -- Operator
+		reg_c.k and "Constants[:C:+1]" or "Stack[:C:]"  -- Check 2 (B then)
+	)
+
 	
 	return layout
 end
-
--- Bytecode/OpCodes/33.lua
+++ Bytecode/OpCodes/33.lua
@@ -1,24 +1,19 @@
--- FORPREP
-return function(instruction,shiftAmount,constant,settings)
-	local layout = [[
+-- TFORPREP
+return [[
+	local A = :A:
+	local C = :C:
+	local Result = {
+		Stack[A](Stack[A + 1], 
+		Stack[A + 2])
+	}
 
-local A = :A:
-local C = :C:
-local Result = {Stack[A](Stack[A + 1], Stack[A + 2])}
+	for i = 1, C do
+		Stack[A + 2 +i] = Result[i]
+	end
 
-for i = 1, C do
-	Stack[A + 2 +i] = Result[i]
-end
-
-if Stack[A + 3] ~= nil then
-	Stack[A + 2] = Stack[A + 3]
-	pointer = pointer + 1
-else
-	pointer = pointer + 1
-end
-
-	]]
-	
-	return layout
-end
-
+	if Stack[A + 3] ~= nil then
+		Stack[A +2] = Stack[A + 3]
+	else
+		pointer = pointer + 1
+	end
+]]
